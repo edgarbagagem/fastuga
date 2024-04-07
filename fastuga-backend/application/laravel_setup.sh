9de6fccb78cb07166efe32a3bed5c3bad892da14
Edgar Bagagem <edgarbagagem@gmail.com>
2024-04-07 16:57:06 +0100

    use sql auth proxy

--- fastuga-backend/application/laravel_setup.sh
+++ fastuga-backend/application/laravel_setup.sh
@@ -16,6 +16,7 @@ echo "DB_HOST=$DB_HOST" >> .env
 echo "DB_DATABASE=$DB_DATABASE" >> .env
 echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+echo "DB_SOCKET=$DB_SOCKET" >> .env
 php artisan key:generate
 
 cat .env
